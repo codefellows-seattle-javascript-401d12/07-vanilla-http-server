9179e7477256410fa30c5d6f9562bdb12c1c69c9
Steven Bateman <batemanvo@gmail.com>
2016-12-13 18:05:37 -0800

    Created server and started it via server.listen.

--- lab-steven/server.js
+++ lab-steven/server.js
@@ -6,3 +6,11 @@ const querystring = require('querystring');
 const cowsay = require('cowsay');
 const parseBody = require('./lib/body-parser.js');
 const PORT = process.env.PORT || 8080;
+
+const server = http.createServer(function(request, response) {
+
+});
+
+server.listen(PORT, function() {
+  console.log('Server started on port', PORT);
+});
