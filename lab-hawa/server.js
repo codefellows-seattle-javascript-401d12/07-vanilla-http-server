b70f41c0920b4f5b1845cfdf72ebc1e665ad7496
Hawa Abdi <abdih17@hotmail.com>
2016-12-14 11:27:56 -0800

    Scaffolded files and modules. Initial functionality complete. My code looks a mess right now, but I plan on refactoring my code later today. This is all Ihave for now.

--- lab-hawa/server.js
+++ lab-hawa/server.js
@@ -10,50 +10,69 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer(function(req, res) {
   req.url = url.parse(req.url);
   req.url.query = querystring.parse(req.url.query);
+  // var contentType = {'Content-Type': 'text/plain'};
+  if ('/' === req.url.pathname) {
+    // res.writeHead( cowsay.say(200, contentType));
+    res.write('Hello peeps!');
+    res.end();
+    return;
+  }
+  // if (req.url.pathname === '/cowsay') {
+  //   // res.writeHead( 200, {'Content-Type': 'text/plain'});
+  //   res.write(cowsay.say({ text: 'hello from cowsay!!' }));
+  //   res.end();
+  //   return;
+  // }
 
-  // * For all **GET** requests made to `/cowsay`, the server should respond with the following:
-  if ( req.url.pathname === '/cowsay' && req.method === 'GET') {
-    // the query string should have the key value `text=<message>`
-    if (req.url.query.write({text: 'hello world'})); {
-      //   * the response header should include `Content-Type: text/plain`
-      //   * if the query `text=messsage` is set, respond with:
-      //     * a status code of 200
-      //     * a response body that includes the value returned from `cowsay.say({ text: <querystring text> })`
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.write(cowsay.say({ text: req.url.query}));
-      res.end();
-    }
+  if (req.method === 'GET' && req.url.pathname === '/cowsay') {
+//  if (req.url.query.write({text: 'helloWorld'})); {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(cowsay.say({ text: 'hello from cowville' }));
+    res.end();
+    return;
+  }
     //   * if the query `text=message` is **not** set, respond with:
     //     * status code = 400
     //     * a body including the value returned from `cowsay.say({ text: 'bad request' })`
-    if ( req.url.query ) {
-      res.writeHead(400, {'Content-Type': 'text/plain'});
-      res.write(cowsay.say ({ text: 'bad request'}));
+  // if ( req.body !== {text: 'Hello request body!'} ) {
+  //   res.writeHead(400, {'Content-Type': 'text/plain'});
+  //   res.write(cowsay.say ({ text: 'very bad request'}));
+  //   res.end();
+  //   return;
+  // }
+  //
+  // // TODO: '&&' or '||'??
+  // if (req.method === 'POST' && req.url.pathname === '/cowsay') {
+  //   parseBody(req, function(err) {
+  //     if (err) return console.error(err);
+  //     console.log('POST request body:', req.body);
+  //   });
+  // }
+    // For all **POST** requests made to `/cowsay`, the server should respond with the following:
+    //   * the response header should include `Content-Type: text/plain`
+    //   * if the JSON `{text: messsage}` is set in the body, respond with:
+    //     * a status code of 200
+    //     * a response body including the value returned from `cowsay.say({ text: <querystring text> })` TODO???
+  if (req.method === 'POST'){
+    if (req.url.pathname === '/cowsay') {
+      res.write(cowsay.say({ text: 'hello from the amazing cowsay.' }));
       res.end();
+      return;
     }
+    // if ( req.body === {text: 'Hello request body!'} ) {
+    //   res.writeHead(200, {'Content-Type': 'text/plain'} );
+    //   res.write(cowsay.say({ text: req.url.query.text }));
+    //   res.end();
+    // }
   }
-
-
-  if (req.method === 'POST') {
-    parseBody(req, function(err) {
-      if (err) return console.error(err);
-      console.log('POST request body:', req.body);
-    });
-  }
-
-  if (req.method === 'GET' && req.url.pathname === '/cowsay') {
-    res.writeHead( 200, {'Content-Type': 'text/plain'} );
-    res.write(cowsay.say({ text: 'hello from cowsay.' }));
+  //   * if the JSON `{text: messsage}` is **not** set in the body, respond with:
+  //       * a status code of 400
+  //       * a body including the value returned from `cowsay.say({ text: 'bad request' })`
+  if (req.body !== {text: 'Hello request body!'}) {
+    res.writeHead(400);
+    res.write(cowsay.say({ text: 'Horribly bad request' }));
     res.end();
   }
-
-  if ('/' === req.url.pathname) {
-    res.writeHead( cowsay.say(200, {'Content-Type': 'text/plain'} ));
-    res.write('Hello from my server!');
-    res.end();
-  }
-
-
 
   res.end();
 });
