ba2328eb93ae851a968da75d7ad45fad2d66edba
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-12-14 10:36:13 -0800

    renaming structure titles for learning purpose

--- lab-jinho/server.js
+++ lab-jinho/server.js
@@ -1,17 +1,18 @@
 'use strict';
 
+//**DEPENDENCIES**
 //node modules
 const http = require('http');
 const url = require('url');
 const queryString = require('querystring');
 //npm modules
 const cowsay = require('cowsay');
-//app modules
+//custom modules
 const parseBody = require('./lib/parse-body.js');
-//module constants
+//environment variables
 const PORT = process.env.PORT || 3000;
 
-//module logic
+//**LOGIC**
 const server = http.createServer(function(req, res) {
   req.url = url.parse(req.url);
   req.url.query = queryString.parse(req.url.query);
