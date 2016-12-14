15da0e20cbd67719f2f0f722549229a0489d1449
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-12-14 10:13:05 -0800

    add constants

--- lab-jinho/server.js
+++ lab-jinho/server.js
@@ -1,1 +1,17 @@
 'use strict';
+
+//node modules
+const http = require('http');
+const url = require('url');
+const queryString = require('querystring');
+//npm modules
+const cowsay = require('cowsay');
+//app modules
+const parseBody = require('./lib/parse-body.js');
+//module constants
+const PORT = process.env.PORT || 3000;
+
+//module logic
+const server = http.createServer(function(req, res) {
+  req.url = url.parse(req.url);
+  req.url.query = queryString.parse(req.url.query);
