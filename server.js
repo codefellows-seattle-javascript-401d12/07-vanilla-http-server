2ebb4b8fbb9ff26b376393cc93863f1715b05acb
kcirekcom <erick.f.mock@gmail.com>
2016-12-13 16:31:36 -0800

    created variables and requires, setup server to listen for PORT

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const cowsay = require('cowsay');
+const parseBody = require('./lib/parse-body.js');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(function(req, res) {
+  
+});
+
+server.listen(PORT, () => {
+  console.log('server running:', PORT);
+});
