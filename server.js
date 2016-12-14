6d0b34da677d7ba6c16bdda283f3e8d3961d1b26
kcirekcom <erick.f.mock@gmail.com>
2016-12-13 16:56:46 -0800

    get request is working properly

--- server.js
+++ server.js
@@ -8,7 +8,20 @@ const parseBody = require('./lib/parse-body.js');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(function(req, res) {
-  
+  req.url = url.parse(req.url);
+  req.url.query = querystring.parse(req.url.query);
+
+  // console.log('req url:', req.url);
+  // console.log('req querystring:', req.url.query);
+  //
+  // console.log('request method:', req.method);
+
+  if (req.method === 'GET' && req.url.pathname === '/cowsay') {
+    // console.log('you have made a request!');
+    res.write(cowsay.say({text: 'hey erick'}));
+    res.end();
+  }
+  res.end();
 });
 
 server.listen(PORT, () => {
