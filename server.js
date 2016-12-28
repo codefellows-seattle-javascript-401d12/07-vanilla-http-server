d3dbdf42d69a1027a4a7d7b52d895b1f82a7851d
Bhavya Bharti <bhavyab@outlook.com>
2016-12-28 15:39:07 -0800

    mvp compleate

--- server.js
+++ server.js
@@ -3,17 +3,31 @@
 const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
-const cowsay = require('cowsay');
-const parsBody = require('./lib/parse-body.js');
+const response = require('./lib/response.js');
 const PORT = 3000;
 
 
 const server = http.createServer(function(req,res){
-  console.log('http server created');
   req.url = url.parse(req.url);
   req.url.query = querystring.parse(req.url.query);
-});
 
+
+  if(req.url.path === '/'){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('hello from my server!');
+    res.end();
+  }
+
+  if (req.method === 'POST' && req.url.pathname === '/cowsay') {
+    response(req.url.query, res);
+    res.end();
+  }
+
+  if(req.method === 'GET' && req.url.pathname === '/cowsay'){
+    response(req.url.query, res);
+    res.end();
+  }
+});
 server.listen(PORT, () => {
   console.log('Hello http server');
 });
