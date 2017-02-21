57787e6ddc90632c7c7487007e1d005d5f80ca52
Hawa Abdi <abdih17@hotmail.com>
2017-02-21 10:33:53 -0800

    Removed corpse code, and made minor changes to server file to complete functionality.

--- lab-hawa/server.js
+++ lab-hawa/server.js
@@ -10,73 +10,46 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer(function(req, res) {
   req.url = url.parse(req.url);
   req.url.query = querystring.parse(req.url.query);
-  // var contentType = {'Content-Type': 'text/plain'};
-  if ('/' === req.url.pathname) {
-    // res.writeHead( cowsay.say(200, contentType));
-    res.write('Hello peeps!');
-    res.end();
-    return;
-  }
-  // if (req.url.pathname === '/cowsay') {
-  //   // res.writeHead( 200, {'Content-Type': 'text/plain'});
-  //   res.write(cowsay.say({ text: 'hello from cowsay!!' }));
-  //   res.end();
-  //   return;
-  // }
 
-  if (req.method === 'GET' && req.url.pathname === '/cowsay') {
-//  if (req.url.query.write({text: 'helloWorld'})); {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(cowsay.say({ text: 'hello from cowville' }));
-    res.end();
-    return;
-  }
-    //   * if the query `text=message` is **not** set, respond with:
-    //     * status code = 400
-    //     * a body including the value returned from `cowsay.say({ text: 'bad request' })`
-  // if ( req.body !== {text: 'Hello request body!'} ) {
-  //   res.writeHead(400, {'Content-Type': 'text/plain'});
-  //   res.write(cowsay.say ({ text: 'very bad request'}));
-  //   res.end();
-  //   return;
-  // }
-  //
-  // // TODO: '&&' or '||'??
-  // if (req.method === 'POST' && req.url.pathname === '/cowsay') {
-  //   parseBody(req, function(err) {
-  //     if (err) return console.error(err);
-  //     console.log('POST request body:', req.body);
-  //   });
-  // }
-    // For all **POST** requests made to `/cowsay`, the server should respond with the following:
-    //   * the response header should include `Content-Type: text/plain`
-    //   * if the JSON `{text: messsage}` is set in the body, respond with:
-    //     * a status code of 200
-    //     * a response body including the value returned from `cowsay.say({ text: <querystring text> })` TODO???
-  if (req.method === 'POST'){
-    if (req.url.pathname === '/cowsay') {
-      res.write(cowsay.say({ text: 'hello from the amazing cowsay.' }));
+  if(req.method === 'POST' && req.url.pathname === '/cowsay') {
+    parseBody (req, function(err) {
+      console.log('POST request body:', req.body);
+      if(err) console.error(err);
+      try {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(cowsay.say({text: req.body.text}));
+      } catch(err){
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.write(cowsay.say({text: 'bad request'}));
+      }
       res.end();
-      return;
-    }
-    // if ( req.body === {text: 'Hello request body!'} ) {
-    //   res.writeHead(200, {'Content-Type': 'text/plain'} );
-    //   res.write(cowsay.say({ text: req.url.query.text }));
-    //   res.end();
-    // }
-  }
-  //   * if the JSON `{text: messsage}` is **not** set in the body, respond with:
-  //       * a status code of 400
-  //       * a body including the value returned from `cowsay.say({ text: 'bad request' })`
-  if (req.body !== {text: 'Hello request body!'}) {
-    res.writeHead(400);
-    res.write(cowsay.say({ text: 'Horribly bad request' }));
-    res.end();
+    });
+    return;
   }
 
+  if(req.method === 'GET' && req.url.pathname === '/') {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('hello from my server!');
+  }
+
+  if(req.method === 'GET' && req.url.pathname === '/cowsay' && req.url.query) {
+    if(req.url.query.text ==='dragon') {
+      res.write(cowsay.say({f: 'dragon', text: 'DRAAAAAAGON!!'}));
+      res.end();
+    } else{
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write(cowsay.say({text: req.url.query.text}));
+      res.end();
+    }
+  }
+  else {
+    res.writeHead(400, {'Content-Type': 'text/plain'});
+    res.write(cowsay.say({text: 'bad request'}));
+  }
   res.end();
+  return;
 });
 
-server.listen(PORT, function() {
-  console.log( `Server on: ${PORT}` );
+server.listen(PORT, () => {
+  console.log('Server running on PORT:', PORT);
 });
