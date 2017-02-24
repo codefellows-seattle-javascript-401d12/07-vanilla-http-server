dbe0af5e7befc812d8d3d0adb1351a78b49218ad
Caleb Sattgast <calebsattgast@gmail.com>
2017-02-24 15:32:15 -0800

    set up basic app dependencies, set up simple http server

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 'use strict';
 
 const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const cowsay = require('cowsay');
+const PORT = process.env.PORT || 8000;
 
-// BUILD OUT REST OF SERVER
+const server = http.createServer(function(req, res) {
+  // TODO: BUILD OUT REST OF SERVER
+});
+
+server.listen(PORT, () => {
+  console.log(`SERVER RUNNING ON PORT ${PORT}.`);
+});
