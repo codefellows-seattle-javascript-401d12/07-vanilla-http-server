e963146e671870ea664701b0655000185a3d614a
Caleb Sattgast <calebsattgast@gmail.com>
2017-02-24 15:37:45 -0800

    build out GET request handling

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(function(req, res) {
   // TODO: BUILD OUT REST OF SERVER
+
+  req.url = url.parse(req.url);
+  req.url.query = querystring.parse(req.url.query);
+
+  if (req.method === 'GET' && req.url.pathname === '/cowsay') {
+    console.log('get request to cowsay');
+    res.write(cowsay.say({text: 'hello from cowville'}));
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
