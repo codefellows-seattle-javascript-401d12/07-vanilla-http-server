62e661e79669296a8b32c108af30a4927a59e6d8
Caleb Sattgast <calebsattgast@gmail.com>
2017-02-24 23:13:18 -0800

    build out more request handlers

--- server.js
+++ server.js
@@ -8,20 +8,73 @@ const parseBody = require('./lib/parse-body.js');
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(function(req, res) {
-  // TODO: BUILD OUT REST OF SERVER
 
   req.url = url.parse(req.url);
   req.url.query = querystring.parse(req.url.query);
 
-  if (req.method === 'GET' && req.url.pathname === '/cowsay') {
+  // REQUESTS TO ROOT URL
+  if (req.method === 'POST' && req.url.pathname === '/') {
+    console.log('successful request to home');
+    res.writeHead(200,
+      {
+        'Content-Type': 'text/plain',
+      }
+    );
+    res.write('hello from my server!');
+    res.end();
+  }
+
+  if (req.method === 'GET' && req.url.pathname === '/') {
+    console.log('successful request to home');
+    res.writeHead(200,
+      {
+        'Content-Type': 'text/plain',
+      }
+    );
+    res.write('hello from my server!');
+    res.end();
+  }
+
+  // REQUESTS TO COWSAY URL
+  if (req.method === 'GET' && req.url.pathname === '/cowsay' && !req.url.query.text) {
+    res.writeHead(200,
+      {
+        'Content-Type': 'text/plain',
+      }
+    );
     console.log('get request to cowsay');
     res.write(cowsay.say({text: 'hello from cowville'}));
     res.end();
   }
 
-  if (req.method === 'POST') {
+  if (req.method === 'GET' && req.url.pathname === '/cowsay' && req.url.query.text) {
+    res.writeHead(200,
+      {
+        'Content-Type': 'text/plain',
+      }
+    );
+    res.write(cowsay.say({text: req.url.query.text}));
+    res.end();
+  }
+
+  if (req.method === 'POST' && req.url.pathname === '/cowsay' && req.url.query.text) {
     parseBody(req, function(err) {
-      console.log(`POST request body: ${req.body}`);
+      console.log(`POST request body: ${req.url.query.text}`);
+      res.writeHead(200,
+        {
+          'Content-Type': 'text/plain',
+        }
+      );
+      res.write(cowsay.say({text: req.url.query.text}));
+      res.end();
+    });
+  }
+
+  if (req.method === 'POST' && req.url.pathname === '/cowsay' && !req.url.query.text) {
+    parseBody(req, function(err) {
+      res.writeHead(400);
+      res.write(cowsay.say({text: 'BAD REQUEST.'}));
+      res.end();
     });
   }
 });
